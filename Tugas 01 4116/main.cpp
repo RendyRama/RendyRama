a78de0030aebaadee2faca3e6df1ebaf8d357d33
RendyRama 4116 14590 <113005915+RendyRama@users.noreply.github.com>
2022-09-11 21:03:29 +0700

    Add files via upload

---
+++ Tugas 01 4116/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Hello world! \nNama : Muhammad Rendy Ramadhan \nNIM : A11.2022.14590 \nAsal : Kabupaten Tegal\nHobi : Bermain komputer" << endl;
+
+    return 0;
+}
