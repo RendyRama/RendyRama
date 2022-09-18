461c0849803a74bca264ef8d03691e8707b710cf
RendyRama 4116 14590 <113005915+RendyRama@users.noreply.github.com>
2022-09-18 10:45:42 +0700

    Add files via upload

---
+++ Tugas 02 4116/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Biodata" << endl;
+    /*printf ("teks\n);
+    Nama    : Muhammad Rendy Ramadhan
+    Nim : A11.2022.14590
+    */
+
+string nama;
+    cout << "masukkan nama anda : ";
+    cin >> nama;
+
+    cout << "halo " << nama;
+    cout << endl;
+    cout << "selamat datang di biodataku!" << endl;
+
+
+
+
+int a = 18;
+char b = 'L';
+string c = "XL";
+string h = "Muhammad Rendy Ramadhan";
+
+    cout << "Perkenalkan nama saya : " << h;
+    cout << endl;
+    cout << "umur saya : " << a;
+    cout << endl;
+    cout << "ukuran celana : " << b;
+    cout << endl;
+    cout << "ukuran baju : " << c;
+    cout << endl;
+
+    int d;
+    cout << "masukkan angka anda : ";
+    cin >> d;
+    cout << "angka anda + 18: " << d + a;
+    cout << endl;
+
+return 0;
+}
