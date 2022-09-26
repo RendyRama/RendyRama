d32073645c45f43b008e2e95b4ab26a5779c9959
RendyRama 4116 14590 <113005915+RendyRama@users.noreply.github.com>
2022-09-26 19:01:46 +0700

    Add files via upload

---
+++ Tugas 03 4116/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float p, l;
+
+    cout << "Perhitungan Luas Persegi Panjang " << endl;
+    cout << "Masukkan panjang : ";
+    cin >> p;
+    cout << "Masukkan lebar : ";
+    cin >> l;
+    cout << "Hasil luas : " << p*l;
+
+    return 0;
+}
